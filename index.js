a7886139667e9586feed4780e5a8f09815e7210a
Alberto <ajsaenz1091@gmail.com>
2021-04-26 19:47:20 -0400

    commit tree 5ee617a509c8a5cc71414c2be02232a0ed361149

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 let currentUser = 'Grace Hopper';
-let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 let excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser + "!";
-let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+let shortGreeting = `Welcome, ${currentUser.split('').splice(0,1).join("")}!`;
